6b21af19cd3a2bb8553bc28a976570b3f01893aa
ZaArs <zaars.x@gmail.com>
2021-01-19 20:48:31 +0300

    Инизиализация Модели

---
+++ resizer/models.py
@@ -0,0 +1,69 @@
+from django.core import validators
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class SourceImage(models.Model):
+
+    class Meta:
+        verbose_name = _('source image')
+        verbose_name_plural = _('source images')
+
+        ordering = ['title']
+
+    title = models.CharField(
+        verbose_name=_('title'),
+        max_length=255,
+        db_index=True,
+    )
+
+    uploaded = models.DateTimeField(
+        verbose_name=_('uploaded'),
+        auto_created=True,
+    )
+
+    image = models.ImageField(
+        verbose_name=_('image'),
+        upload_to='upload/source_images',
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class ModifiedImage(models.Model):
+
+    class Meta:
+        verbose_name = _('modified image')
+        verbose_name_plural = _('modified images')
+
+        ordering = ['source_image__title', 'width', 'height']
+
+    source_image = models.ForeignKey(
+        verbose_name=_('source image'),
+        to=SourceImage,
+        on_delete=models.CASCADE,
+    )
+
+    width = models.PositiveIntegerField(
+        verbose_name=_('image width'),
+        validators=[
+            validators.MinValueValidator(0),
+        ]
+    )
+
+    height = models.PositiveIntegerField(
+        verbose_name=_('image height'),
+        validators=[
+            validators.MinValueValidator(0),
+        ]
+    )
+
+    image = models.ImageField(
+        verbose_name=_('image'),
+        upload_to='upload/modified_images',
+    )
+
+    def __str__(self):
+        return f'{self.source_image} [{self.width} x {self.height}]'
+
